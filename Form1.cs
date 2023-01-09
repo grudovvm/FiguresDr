0c1aa9bc6af2db116e3ab16f303fc7bf5beabe61
grudovvm <mariogrudov6@gmail.com>
2023-01-09 16:18:55 +0200

    Added function to circle button

--- Form1.cs
+++ Form1.cs
@@ -11,7 +11,20 @@ public partial class Form1 : Form
 
     private void btnCircle_Click(object sender, EventArgs e)
     {
-        
+        Thread thread2 = new Thread(CircleThread);
+        thread2.Start();
+    }
+
+    public void CircleThread()
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            Random random = new Random();
+            Pen pen = new Pen(Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255)));
+            Graphics circle = this.CreateGraphics();
+            circle.DrawEllipse(pen, random.Next(0, this.Width - 155), random.Next(0, this.Height - 155), random.Next(50, 150), random.Next(50, 150));
+            Thread.Sleep(4000);
+        }
     }
     public void RectangleThread()
     {
